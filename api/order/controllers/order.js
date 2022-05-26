7e7398f42a074cfd19e8bdf86a0f9f423aca8b12
Carlos Santander <carlos.santander@gmail.com>
2022-05-25 23:48:51 -0500

    api strapi finalizada

---
+++ api/order/controllers/order.js
@@ -0,0 +1,44 @@
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51HkF5rLlg8czBHMMFlqiM2GuTBwxSas9alX8VdVBe8wk9hO2y98qGn6LAyaZI1d5v9s5ji5rEzR915TUcBv8MxPU009hQVEM7Q"
+);
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async create(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+    products.forEach((product) => {
+      totalPayment = totalPayment + product.price;
+    });
+
+    const charge = await stripe.charges.create({
+      amount: totalPayment * 100,
+      currency: "eur",
+      source: token.id,
+      description: `ID Usuario: ${idUser}`,
+    });
+
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        game: product.id,
+        user: idUser,
+        totalPayment,
+        idPayment: charge.id,
+        addressShipping,
+      };
+      const validData = await strapi.entityValidator.validateEntity(
+        strapi.models.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+    return createOrder;
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c34c5f0a-8324-4abd-9e57-a26e32c2ebc4'
+};
